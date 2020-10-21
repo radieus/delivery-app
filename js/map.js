525fc5cd36040f94be905f56bf6f65f28cf3adbb
Radosław Dubiel <radieus@gmail.com>
2020-10-21 20:47:30 +0200

    Adds popups

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([52.22992817667709, 21.00809365510941], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -8,13 +8,27 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoicmFkaWV1cyIsImEiOiJja2dmNjNneTAwdWxwMnZxejY1aGRkdm03In0.FS9_5BuYbcxDQWSTseVO3A'
 }).addTo(mymap);
 
-var popup = L.popup();
+var template = '<form data-persist="garlic">\
+    <input type="text" id="Ifrom" name="Ifrom" placeholder="Pickup Address"required><br>\
+    <input type="text" id="Ito" name="Ito" placeholder="Destination Address" required><br>\
+    <input type="date" id="IDate" name="IDate" placeholder="Date" required><br>\
+    <input type="text" id="Itext" name="Itext" placeholder="Details"><br>\
+    <input type="text" id="Iname" name="Iname" placeholder="Name" required><br>\
+    <input type="text" id="Isurname" name="Isurname" placeholder="Surame" required><br>\
+    <input type="email" id="Imail" name="Imail" placeholder="Email" required><br>\
+    <input type="tel" id="Iphone" name="Iphone" placeholder="Phone" required>\
+<button id="btnSubmit">Submit</button>\
+</form>'
 
 var markerArray = [];
 
 function onMapClick(e) {
     var marker = L.marker(e.latlng).addTo(mymap);
     markerArray.push(marker)
+    marker.bindPopup(template, {
+        maxWidth: "auto"
+      });
+      marker.openPopup;
 }
 
 function showNewPackageForm() {
