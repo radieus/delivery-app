66fd7f390b01274ca48b1ca72c3f0e89fd4c882a
Radosław Dubiel <radieus@gmail.com>
2020-10-23 17:14:39 +0200

    Adds stuff

--- js/map.js
+++ js/map.js
@@ -9,10 +9,14 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 var template = '<form data-persist="garlic" action="#" onsubmit="addPoint(); return false">\
-    <input type="text" id="Ifrom" name="Ifrom" placeholder="Pickup Address" required><br>\
-    <input type="text" id="Ito" name="Ito" placeholder="Destination Address" ><br>\
     <input type="date" id="IDate" name="IDate" placeholder="Date" ><br>\
     <input type="text" id="Itext" name="Itext" placeholder="Details"><br>\
+    <input type="number" id="Iweight" name="Iweight" placeholder="Weight [kg]"><br>\
+    <select name="Isize" id="Isize">\
+        <option>Small</option>\
+        <option>Medium</option>\
+        <option>Large</option>\
+    </select>\
     <input type="text" id="Iname" name="Iname" placeholder="Name" ><br>\
     <input type="text" id="Isurname" name="Isurname" placeholder="Surame" ><br>\
     <input type="email" id="Imail" name="Imail" placeholder="Email" ><br>\
@@ -21,8 +25,11 @@ var template = '<form data-persist="garlic" action="#" onsubmit="addPoint(); ret
 </form>';
 
 var markers = L.layerGroup();
-
+var markersDict = {};
 var tmpMarker;
+
+var marker;
+var clickedMarker;
 
 function onMapClick(e) {
     if (tmpMarker) {
@@ -31,7 +38,7 @@ function onMapClick(e) {
     
     tmpMarker = L.marker(e.latlng, {
         draggable: true
-    }).addTo(mymap);
+    }).addTo(mymap); // on('click', onClick)
 
     markers.addLayer(tmpMarker);
     
@@ -45,16 +52,58 @@ function onMapClick(e) {
         var marker = event.target;
         tmpMarker.openPopup();
     });
-
-
 }
 
+function getValueFromFormOf(marker, elem) {
+    return marker.getPopup().getElement().getElementsByTagName("form")[0].getElementsByTagName("input")[elem].value
+}
+
+function setValueFromFormOf(marker, elem, val) {
+    marker.getPopup().getElement().getElementsByTagName("form")[0].getElementsByTagName("input")[elem].value = val;
+}
 
 function addPoint() {
-    tmpMarker.closePopup();
-    markers.addLayer(tmpMarker);
-    tmpMarker = null;
-    console.log("Add point")
+
+    try {
+        markersDict[tmpMarker._leaflet_id] = {
+        "IDate" : getValueFromFormOf(tmpMarker, 0),
+        "Itext" : getValueFromFormOf(tmpMarker, 1),
+        "Iname" : getValueFromFormOf(tmpMarker, 2),
+        "Isurname" : getValueFromFormOf(tmpMarker, 3),
+        "Imail" : getValueFromFormOf(tmpMarker, 4),
+        "tel" : getValueFromFormOf(tmpMarker, 5)
+        };
+
+        console.log(markersDict);
+
+        tmpMarker.closePopup();
+        markers.addLayer(tmpMarker);
+        tmpMarker = null;
+        console.log("Add point");
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+
+// function onClick(event) {
+//     marker = event.sourceTarget;
+//     clickedMarker = event.originalEvent.path[0];
+
+//     console.log(event)
+//     for(var item in markersDict) {
+//         console.log(marker._leaflet_id);
+//         console.log(item);
+//         if(marker._leaflet_id == item) {
+//             setValueFromFormOf(marker, 0, item["Idate"]);
+//             setValueFromFormOf(marker, 1, item["Itext"]);
+//             setValueFromFormOf(marker, 2, item["Iname"]);
+//             setValueFromFormOf(marker, 3, item["Isurname"]);
+//             setValueFromFormOf(marker, 4, item["Imail"]);
+//             setValueFromFormOf(marker, 5, item["tel"]);
+//         } 
+//     }
+// };
+
 
 mymap.on('click', onMapClick);
